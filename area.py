990e446873a576e71b6feef79ec5baff2a430d27
Amit-cyber-alt <96493564+Amit-cyber-alt@users.noreply.github.com>
2022-03-08 16:35:44 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,27 @@
+#area and circumference of circle
+r = int(input("Enter radius"))
+circum = 4*3.14*r
+area = 3.14*r*r
+print(circum)
+print(area)
+
+
+#area of square
+s = int(input("Side ="))
+area = s*s
+print(area)
+
+
+#area of rectangle
+l = int(input("length ="))
+b = int(input("breadth ="))
+area = l*b
+print(area)
+
+
+#area of trapezoid
+a = int(input("base a ="))
+b = int(input("base b ="))
+h = int(input("heigth h ="))
+area = ((a+b)*h)/2
+print(area)
