917b9fb5b61d211a002a241611867bf29a4fef35
Amit-cyber-alt <96493564+Amit-cyber-alt@users.noreply.github.com>
2022-03-08 15:48:55 +0530

    Update Arthimetic_operations.py

--- Arthimetic_operations.py
+++ Arthimetic_operations.py
@@ -1,5 +1,10 @@
 a = int(input("1st number : " ))
 b = int(input("2nd number : " ))
 c = int(input("3rd number : " ))
+
 sum = a+b+c
 print("Sum of the numbers = ", sum)
+
+diff = a-b-c
+print("Difference of numbers =", diff)
+
